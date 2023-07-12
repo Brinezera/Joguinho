6c89172c75754e7e9ecdc9cb90a1eed89f7f89eb
André Baumgartner <87343051+Brinezera@users.noreply.github.com>
2023-07-12 17:46:07 -0300

    Update script.js

--- script.js
+++ script.js
@@ -3,15 +3,19 @@ const pipe = document.querySelector('.pipe');
 const restartButton = document.querySelector('.restart-button');
 const scoreSpan = document.querySelector('.score span');
 let isGameOver = false;
+let isJumping = false; // Variável para controlar se o Mario está pulando
 let score = 0;
 
 const jump = () => {
-  if (isGameOver) return;
-  
+  if (isGameOver || isJumping) return;
+
+  isJumping = true;
+
   mario.classList.add('jump');
 
   setTimeout(() => {
     mario.classList.remove('jump');
+    isJumping = false;
   }, 500);
 };
 
@@ -51,6 +55,12 @@ const loop = setInterval(() => {
 document.addEventListener('keydown', (event) => {
   if (event.code === 'Space') {
     jump();
+  }
+});
+
+document.addEventListener('keyup', (event) => {
+  if (event.code === 'Space') {
+    isJumping = false;
   }
 });
 
